a4d85442c56dd17bb6475b18d08518a495a8fdaf
Pedro_Mesquita <127952505+Pedro090904@users.noreply.github.com>
2025-05-09 14:23:07 -0300

    Update aula.js
    Complemento do  desafio

--- aula.js
+++ aula.js
@@ -1,7 +1,26 @@
-const PrecoCombustivel = 6.47;
+/*Faça um programa para calcular o valor de uma viagem.
 
-let gastoCKM;
+Você terá 3 variaveis. Send elas:
 
-let distancia;
+1- Preço do combustivel;
+2- Gasto médio de combustivel do carro por km;
+3- Distância em KM da viagem ;
 
-console.log('teste');
+Imprima no console o valor que sera gasto de combustivel para realizar esta viagem. */
+
+
+const PrecoCombustivel = 5.79;
+const KM_Litros = 10;
+const distancia= 100;
+
+
+const GastoKm = ((distancia/KM_Litros)*PrecoCombustivel);
+
+
+console.log( `o valor é ${GastoKm.toFixed(5)}`);
+
+let valorA = 5;
+
+let valorB = '10';
+
+console.log(valorA + valorB);
