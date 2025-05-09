4ea6521506d981889aeaede7383529710cd8bffa
Pedro_Mesquita <127952505+Pedro090904@users.noreply.github.com>
2025-05-09 15:58:49 -0300

    Create aula.js
    encaminhado para uma pasta

---
+++ 01 - Variaveis e operadores/aula.js
@@ -0,0 +1,22 @@
+/*Faça um programa para calcular o valor de uma viagem.
+
+Você terá 3 variaveis. Send elas:
+
+1- Preço do combustivel;
+2- Gasto médio de combustivel do carro por km;
+3- Distância em KM da viagem ;
+
+Imprima no console o valor que sera gasto de combustivel para realizar esta viagem. */
+
+
+const PrecoCombustivel = 5.79;
+const KM_Litros = 1;
+const distancia= 100;
+
+
+const GastoKm = ((distancia/KM_Litros)*PrecoCombustivel);
+
+
+console.log( `o valor é ${GastoKm.toFixed(2)}`);
+
+
