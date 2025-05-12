69d82dc4090c5ab460d425d407d6aaaa37bf4c5e
Pedro_Mesquita <127952505+Pedro090904@users.noreply.github.com>
2025-05-12 15:36:59 -0300

    Update aula02.js

--- 02 - Condicionais/aula02.js
+++ 02 - Condicionais/aula02.js
@@ -1,13 +1,30 @@
 
-const numero = 63;
+const numero = 0;
 
 const eNumeroPar = (numero % 2) === 0;
-const NumeroImpar = (numero % 2) > 0;
+const NUMERO_DIVISIVEL_5 = (numero % 5) ===0;
 
-if(eNumeroPar){
-console.log('É par');
+if(numero === 0){
+    console.log(`O numero é invaldio`);
+} 
+
+else{
+
+    if(eNumeroPar){
+    console.log(`O numero ${numero} é par!`);
+    }
+
+    if(!eNumeroPar){
+    console.log(`O numero ${numero} é impar!`);
+    }
+
+    if(NUMERO_DIVISIVEL_5){
+    console.log(`SIM, o numero ${numero} é divisivel por 5`);
+    }
+
+    else{
+    console.log(`NÃO é divisive por 5, pois o numero é: ${numero}`);
+    }
 }
 
-if(!eNumeroPar){
-console.log('O numero é impar');
-}
+
