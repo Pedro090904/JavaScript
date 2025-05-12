6834c94f78392a31678fdb08071b104195f7b90f
Pedro_Mesquita <127952505+Pedro090904@users.noreply.github.com>
2025-05-12 16:36:47 -0300

    Create IMCCalculadora.js

---
+++ 03 - Exercicios/IMCCalculadora.js
@@ -0,0 +1,17 @@
+/*
+2) O IMC - Indice de Massa Corporal é um criterio da Organização Mundial de Saúde
+ para dar uma indicaçao sobre a condição de peso de uma pessoa adulta.
+
+ Formula IMC: IMC = peso/ (altura * altura)
+
+ Elabore um algoritimo que dado o peso e a altura de um adulto mostre sua condição de acordo 
+ com a atabela a baixo:
+
+ IMC em adultos:
+ - Abaixo de 18.5, Abaixo do peso;
+ - Entre 18.5 e 25, peso normal;
+ - Entre 25 e 30, acima do peso;
+ - Entre 30 e 40, obeso;
+ - Acima de 40, Obesidade grave;
+*/
+
