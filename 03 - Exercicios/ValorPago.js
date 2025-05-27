4af7ea0a52698ffe8e3a500cd46bca13c09963ee
Pedro_Mesquita <127952505+Pedro090904@users.noreply.github.com>
2025-05-27 16:37:26 -0300

    Update ValorPago.js

--- 03 - Exercicios/ValorPago.js
+++ 03 - Exercicios/ValorPago.js
@@ -1,6 +1,21 @@
+/*
+3 Elabore um algoritimo que calcule o que deve ser pago por um produto, considerando o preço
+normal de etiqueta e a escolha da condição do pagamento.
+
+
+Utilize os códigos da tabela a seguir para ler qual a condição de pagamento escolhida e efetuar
+o calculo adequado.
+
+
+Código Condição de pagamento:
+- A vista no Débito, recebe 10% de desconto;
+- A vista no dinheiro ou pix, recebe 15% de desconto;
+- Em duas vezes, preço normal de etiqueta mais juros de 10%;
+*/
 
 
 const preco = 100; 
+
 const tipoPagamento = 3;
 
 
@@ -13,7 +28,7 @@ else if(tipoPagamento === 2){
 else if(tipoPagamento ===3){
     console.log(`O valor em duas vezes será de ${preco}$, com cada parcela por ${preco/2}$`)
 }
+
 else{
     console.log(`O preço dividido em duas será: ${preco +(preco*0.1)}`);
 }
-
