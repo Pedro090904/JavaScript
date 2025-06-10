f38a7e0a732c463f075c4c087cd0e37b91506f6a
Pedro_Mesquita <127952505+Pedro090904@users.noreply.github.com>
2025-06-10 16:54:58 -0300

    Update EscreverNome.js

--- 04 - Funções/EscreverNome.js
+++ 04 - Funções/EscreverNome.js
@@ -2,5 +2,30 @@ function escreverNome(nome){
     console.log (`O meu nome é ${nome}`)
 }
 
+function verificaIdade(idade){
+    if(idade >= 18){ 
+        console.log(`Maior de idade`);
+    }
+    else{
+        console.log(`Menor de idade`);
+    }
+}
+
+
 escreverNome('Pedro de Sousa Mesquita');
+verificaIdade(21);
 escreverNome('joão neto');
+verificaIdade(10);
+
+
+function testeNome(Nome){
+    return `Meu nome é ${Nome}`;
+}
+
+function testeIdade(Idade){
+    if(Idade >= 18){
+        console.log(testeNome)
+    }
+}
+
+
