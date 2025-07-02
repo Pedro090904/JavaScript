2aaaa4272acb1c425920ac006d4861cb4019157c
Pedro_Mesquita <127952505+Pedro090904@users.noreply.github.com>
2025-07-01 23:06:07 -0300

    Update FuncaoF.js

--- 04 - Funções/FuncaoF.js
+++ 04 - Funções/FuncaoF.js
@@ -1,2 +1,17 @@
 
 
+testarIdade(34);
+
+function testarIdade(idade){
+    if(idade >= 18){
+        console.log(`A pessoa é maior de idade`);
+    }
+    else 
+    {
+        console.log(`Apessoa é menor de idade`);
+    }
+}
+
+
+
+
